86afedf6bf57484ea11c1e0e8072da28a35fb95f
unknown <devethancraig@gmail.com>
2019-02-05 12:01:21 -0700

    Alerts Working - Email Alert in Progress

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -22,7 +22,11 @@ export class Form extends Component {
     const {name, email, message} = this.state;
     const lead = {name, email, message};
     this.props.addLead(lead);
-    console.log("Submit");
+    this.setState({
+      name: "",
+      email: "",
+      message: ""
+    });
   }
 
   render() {
