96b9c543370f78d82724f179e9855b9f5805c537
unknown <devethancraig@gmail.com>
2019-02-06 22:15:28 -0700

    Created Register - Login - Logout on the Backend

--- leadmanager/frontend/src/actions/messages.js
+++ leadmanager/frontend/src/actions/messages.js
@@ -1,8 +1,15 @@
-import {CREATE_MESSAGE } from './Types';
+import {CREATE_MESSAGE, GET_ERRORS } from './Types';
 
 export const createMessage = msg => {
     return {
         type: CREATE_MESSAGE,
         payload: msg
+    }
+}
+
+export const returnErrors = (msg, status) => {
+    return {
+        type: GET_ERRORS,
+        payload: {msg, status}
     }
 }
